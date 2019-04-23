cbcb49c76314052de671eb9db500607934dfa944
aqurilla <aqurilla@gmail.com>
2019-04-23 15:01:00 -0400

    Initial

---
+++ Wrapper.py
@@ -0,0 +1,153 @@
+#!/usr/bin/evn python
+
+"""
+CMSC733 Spring 2019: Classical and Deep Learning Approaches for
+Geometric Computer Vision
+Project 3: Buildings built in minutes - An SfM Approach
+
+References:
+
+"""
+
+# Code starts here:
+
+# Imports
+import numpy as np
+import cv2
+import argparse
+import pdb
+import glob
+from misc.utils import *
+from misc.helpers import *
+from scipy.optimize import least_squares
+
+
+# Camera intrinsic params
+K = np.array([[568.996140852, 0, 643.21055941],
+              [0, 568.988362396, 477.982801038],
+              [0, 0, 1]])
+
+def main():
+	# Add any Command Line arguments here
+    Parser = argparse.ArgumentParser()
+    #Parser.add_argument('--imnum_1', default=1, help='Image number of 1st image')
+    #Parser.add_argument('--imnum_2', default=2, help='Image number of 2nd image')
+    Parser.add_argument('--txtpath', default='../Data/matching', help='Prefix path to correspondence files')
+
+    Args = Parser.parse_args()
+    #imnum_1 = int(Args.imnum_1)
+    #imnum_2 = int(Args.imnum_2)
+    txtpath = Args.txtpath
+
+    # X0_nloptim, residual, F_RANSAC, E_recon, C, R, P = Estimate3DPoints(imnum_1, imnum_2, txtpath)
+
+    '''
+    Perform outlier rejection via RANSAC and estimate the fundamental matrix
+    '''
+    # Parse correspondence points between the first two images
+    points_mat = parsePoints(1, 2, txtpath)
+
+    points_RANSAC = GetInlierRANSAC(points_mat)
+
+    # Fundamental matrix using inliers
+    F_RANSAC = EstimateFundamentalMatrix(points_RANSAC)
+
+    '''
+    Estimate essential matrix from the fundamental matrix
+    '''
+    E_recon = EssentialMatrixFromFundamentalMatrix(F_RANSAC, K)
+
+    '''
+    Estimate camera pose from essential matrix
+    '''
+    umat, smat, vmat = np.linalg.svd(E_recon)
+
+    W = np.array([[0, -1, 0],
+                  [1, 0, 0],
+                  [0, 0, 1]])
+
+    C1 = umat[:,2]
+    R1 = np.matmul(np.matmul(umat, W), vmat)
+
+    C2 = -1*umat[:,2]
+    R2 = np.matmul(np.matmul(umat, W), vmat)
+
+    C3 = umat[:,2]
+    R3 = np.matmul(np.matmul(umat, W.T), vmat)
+
+    C4 = -1*umat[:,2]
+    R4 = np.matmul(np.matmul(umat, W.T), vmat)
+
+    # Use C and R to calculate camera pose P
+    # P = KR[I(3x3) -C]
+
+    # P0 is the reference Camera
+    P0 = ExtractCameraPose(K,np.zeros(C1.shape), np.identity(R1.shape[0]))
+
+    P1 = ExtractCameraPose(K,C1,R1)
+    P2 = ExtractCameraPose(K,C2,R2)
+    P3 = ExtractCameraPose(K,C3,R3)
+    P4 = ExtractCameraPose(K,C4,R4)
+
+    # Identify the correct camera pose out of the 4 choices
+    '''
+    Linear triangulation to get 3D points
+    '''
+    # Get the reconstructed 3D points corresponding to each pose
+    X1 = LinearTriangulation(P0, P1, points_RANSAC)
+    X2 = LinearTriangulation(P0, P2, points_RANSAC)
+    X3 = LinearTriangulation(P0, P3, points_RANSAC)
+    X4 = LinearTriangulation(P0, P4, points_RANSAC)
+
+    Cset = (C1, C2, C3, C4)
+    Rset = (R1, R2, R3, R4)
+    Xset = (X1, X2, X3, X4)
+
+    # Check the cheirality condition to identify correct pose
+    # C, R, X0 = DisambiguateCameraPose(Cset, Rset, Xset)
+    C, R, X0 = DisambiguateCameraPose_2Check(Cset, Rset, Xset, K, points_RANSAC)
+    P = ExtractCameraPose(K,C,R)
+
+    '''
+    Non-linear optimization of triangulation
+    '''
+    # X0_nl are the non-linearly optimized points
+    X0_nl, residual = NonlinearTriangulation(X0, P0, P, points_RANSAC, max_nfev=100)
+
+    Cset = []
+    Rset = []
+    Cset.append(C)
+    Rset.append(R)
+
+    im2world = impts2wpts(dict(),1,X0_nl,points_RANSAC[:,:2])
+    im2world = impts2wpts(im2world,2,X0_nl,points_RANSAC[:,2:])
+    
+    for i in range(2,6):
+        points_mat = parsePoints(i, i+1, txtpath) # Parse points from matchingi.txt
+        points_RANSAC = GetInlierRANSAC(points_mat)
+        X,x = getWorldPts(im2world,i,points_RANSAC)
+
+        X_RANSAC, x_RANSAC , RC = PnPRANSAC(X, x, K) # What are X and x to input here?
+        RC_nl = NonlinearPnP(X_RANSAC, x_RANSAC, K, RC)
+        Rnew = RC_nl[:,:3]
+        Cnew = RC_nl[:,-1]
+        Cset.append(Cnew)
+        Rset.append(Rnew)
+
+        Pnew = ExtractCameraPose(K,Cnew,Rnew)
+        Xnew = LinearTriangulation(P0, P, points_RANSAC)
+        Xnew_nl, residual = NonlinearTriangulation(Xnew, P0, Pnew, points_RANSAC, max_nfev=100)
+        im2world = impts2wpts(im2world,i+1,X,x)
+        # X = np.append(X, Xnew_nl, axis=0)
+
+    # BuildVisibilityMatrix
+
+    V,X,x = BuildVisibilityMatrix(6,im2world)
+
+    # BundleAdjustment
+    newcams, newpts, info = BundleAdjustment(X,x,K,CRCs,V)
+
+    pdb.set_trace()
+
+if __name__ == '__main__':
+    main()
---
+++ misc/helpers.py
@@ -0,0 +1,128 @@
+import numpy as np
+from random import sample,choice
+from scipy.optimize import leastsq
+from cv2 import Rodrigues
+from scipy.spatial.transform import Rotation as Rot
+import sba
+
+
+def LinearPnP(X,x,K):
+	# X is 4x6, x is 3x6, K ix 3x3
+	RC = np.linalg.lstsq(X.transpose(),np.matmul(np.linalg.inv(K),x).transpose(),rcond=None)[0].transpose()
+	R = RC[:,:-1]
+	if np.linalg.det(R) < 0:
+		R = -R
+		RC[:,:-1] = R
+	RC[:,-1] = np.matmul(-R.transpose(),RC[:,-1])
+	return RC
+
+def impts2wpts(curDict,camidx,newX,newx):
+	for idx,pt in enumerate(newx):
+		curDict[(camidx,pt[0],pt[1])] = newX[idx,:]
+	return curDict
+
+def getWorldPts(im2world,fromIdx,ptCorresps):
+	x = np.empty([2,0])
+	X = np.empty([3,0])
+	for ptc in ptCorresps:
+		tup = (fromIdx,ptc[0],ptc[1])
+		if tup in im2world:
+			x= np.append(x,ptc[2:])
+			X= np.append(X,im2world[tup])
+	return X,x
+
+def reprojErr(X,x,P):
+	# X is 4xN, x is 2xN, P is 3x3
+	PX = np.matmul(P,X)
+	fracs = np.vstack([PX[0,:]/PX[2,:],PX[1,:]/PX[2,:]])
+	return sum((x - fracs)**2,0)
+
+def PnPRANSAC(X,x,K,eps=0.01,numIters=1000):
+	# X is 3xN, x is 2xN, K is 3x3
+	l = X.shape[1]
+	sampleids = range(l)
+	Xhat = np.vstack([X,np.ones([1,l])])
+	xhat = np.vstack([x,np.ones([1,l])])
+	bestInliers = np.zeros(l)
+	bestRC = np.zeros([3,4])
+	for _ in range(numIters):
+		pointids = sample(sampleids,6)
+		Xs = Xhat[:,pointids]
+		xs = xhat[:,pointids]
+		print(Xs.shape,xs.shape)
+		RC = LinearPnP(Xs,xs,K)
+		P = np.matmul(K,RC*np.array([1,1,1,-1]))
+		inliers = np.less(reprojErr(Xhat,x,P),eps)
+		if sum(inliers) > sum(bestInliers):
+			bestInliers = inliers
+			bestRC = RC
+	return X[:,bestInliers],x[:,bestInliers],bestRC
+
+def NonlinearPnP(X,x,K,RC):
+	# X is 3xN, x is 2xN, K is 3x3, RC is 3x4
+	Xhat = np.vstack([X,np.ones([1,X.shape[1]])])
+	C = RC[:,-1]
+	R = RC[:,:-1]
+	def obj(qc,X,x):
+		R = q2R(qc[:4])
+		C = qc[4:].reshape([3,1])
+		return reprojErr(X,x,np.matmul(K,np.concatenate([R,-C],1)))
+	qc = leastsq(obj,np.concatenate([R2q(R),C]),args=(Xhat,x))[0]
+	return np.concatenate(q2R(qc[:4]),rc[4:].reshape([3,1]),1)
+
+def BuildVisibilityMatrix(nCams,im2world):
+	wcs = np.unique(im2world.values())
+	wc2idx = dict(zip([tuple(wc) for wc in wcs],np.arange(wcs.size)))
+	V = np.zeros([wcs.size,nCams])
+	x = np.zeros([wcs.size,nCams,2])
+	for ((camidx,x,y),wc) in im2world.items():
+		row = wc2idx[tuple(wc)]
+		V[row,camidx] = 1
+		x[row,camidx,:] = np.array([x,y])
+	return V,wcs,x
+
+def R2r(R):
+	out,_ = Rodrigues(R,np.zeros([3,1]))
+	return out
+
+def r2R(r):
+	out,_ = Rodrigues(r,np.zeros([3,3]))
+	return out
+
+def r2q(r):
+	robj = Rot.from_rotvec(r)
+	return robj.as_quat()
+
+def q2R(q):
+	robj = Rot.from_quat(q)
+	return r2R(robj.as_rotvec())
+
+def R2q(R):
+	return r2q(R2r(R))
+
+# def camArray2
+
+def Cam2sba(n, CRCs, K):
+    # n - number of cameras
+    sbainp = np.zeros((n, 17))
+
+    sbainp[:,0] = K[0][0] # fx
+    sbainp[:,1] = K[0][2] # cx
+    sbainp[:,2] = K[1][2] # cy
+    sbainp[:,3] = 1       # AR
+    sbainp[:,4] = K[0][1] # s
+
+    for i in range(n):
+        R = (CRCs[:,:,i])[:,:3]
+        C = (CRCs[:,:,i])[:,3]
+        sbainp[:,10:14] = R2q(R)
+        sbainp[:,11:] = Cset[i].reshape(C)
+
+    return sbainp
+
+def BundleAdjustment(X,x,K,CRCs,V):
+	# X is 3xN, x is 2xN, CKs is 3x3xI, CRCs is 3x4xI, V is IxN
+	pts = sba.Points(X,x,V)
+	cams = Cam2sba(6, CRCs, K)
+	# newcams, newpts, info = sba.SparseBundleAdjust(cams,pts)
+	return sba.SparseBundleAdjust(cams,pts)
